64ab467c66f72a1d4ca28c8c4d27656e821b607c
fosa <obaseki321@gmail.com>
2026-05-29 21:12:19 -0700

    slight wording change

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -22,7 +22,7 @@ export function LandingPage() {
           </h1>
           <img src={aboutImage} alt="rankingDemoImage" className="about-image"/>
           <p className="about-info">
-            rankedpool is a casual-competitive ranking platform where players can create profiles, compete against other rankedpool users, and climb the leaderboard through an elo-based ranking system. After each pool match, wether it be 8ball, 9ball or 10ball. Players report wins and losses to gain or lose elo, allowing them to rank up or derank across multiple competitive tiers. Users can track match history, view player profiles, and compare rankings, creating a structured and competitive environment built entirely around matches played between verified members of the platform.
+            rankedpool is a casual-competitive ranking platform where players can compete against other rankedpool users and climb the leaderboard through an elo-based ranking system. After each pool match, wether it be 8ball, 9ball or 10ball; players report wins and losses to gain or lose elo, allowing them to rank up or derank across multiple competitive tiers. Users can track match history, view player profiles, and compare rankings, creating a structured and competitive environment built entirely around matches played between verified members of the platform.
           </p>
         </div>
       </section>
