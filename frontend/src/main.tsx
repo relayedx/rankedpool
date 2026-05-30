83f87e2c5426c127ae8f9550977a683d874eb1cb
fosa <obaseki321@gmail.com>
2026-05-29 21:13:21 -0700

    giving all components access to clerk auth state

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ClerkProvider } from '@clerk/react';
+const CLERK_KEY = import.meta.env.VITE_CLERK_KEY;
+
+if (!CLERK_KEY) {
+  throw new Error("key not found");
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <ClerkProvider publishableKey={CLERK_KEY}>
     <App />
+    </ClerkProvider>
   </StrictMode>
 )
