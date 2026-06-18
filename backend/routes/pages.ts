af8f7c424d5c32784c10556c63a958a3a98da296
fosa. <obaseki321@gmail.com>
2026-06-17 20:43:16 -0700

    changed comment details

--- backend/routes/pages.ts
+++ backend/routes/pages.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import { getUser, createUser } from '../controllers/userController'
 const router = express.Router();
 
-// protected routes
+// routes called from LandingPage
 router.get('/user', getUser);
 router.post('/user/onboarding', createUser);
 
