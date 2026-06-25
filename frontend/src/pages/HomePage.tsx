720567756d9744bd6cf443bd5a776923b282b8f2
fosa. <obaseki321@gmail.com>
2026-06-24 21:23:34 -0700

    added functions for backend api calls and state management to decide rendering

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,8 +1,9 @@
 import { useAuth } from '@clerk/react'
 import { useEffect, useState } from 'react'
 import { OnboardingForm } from '../components/OnboardingForm'
+import { MatchReportForm } from '../components/MatchReportForm'
+import { PendingMatchReport } from '../components/PendingMatchReport'
 import { MenuBar } from '../components/MenuBar'
-import { Link } from 'react-router-dom'
 import './styles/HomePage.css'
 
 type rankedpoolUser = {
@@ -21,7 +22,11 @@ export function HomePage() {
   const [loading, setLoading] = useState<boolean>(true);
   const [needsOnboarding, setNeedsOnboarding] = useState<boolean>(false);
   const [user, setUser] = useState<rankedpoolUser | null>(null);
+  const [reportingMatch, setReportingMatch] = useState<boolean>(false);
+  const [pendingMatchReport, setPendingMatchReport] = useState(null);
+  const [renderUser, setRenderUser] = useState(0);
   
+  // functions for api calls to backend
   const handleCreateUser = async (username: string) => {
     try {
       const token = await getToken();
@@ -37,12 +42,91 @@ export function HomePage() {
       });
 
       const data = await response.json();
+      if (!response.ok) {
+        throw new Error(data.error || 'Failed to create user');
+      }
 
-      setUser(data.user);
+      setUser(data.newUser);
       setNeedsOnboarding(false);
 
     } catch (error) {
       console.log(error);
+    }
+  }
+
+  const handleCreateMatchReport = async (username: string, didWin: boolean, gameType: string) => {
+    try {
+      const token = await getToken();
+      const response = await fetch(`${API_URL}/api/match-report`, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          username,
+          didWin,
+          gameType
+        }),
+      });
+
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error(data.error || 'Failed to create match report');
+      }
+
+      return data;
+      
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleAccept = async (matchReportId: string) => {
+    try {
+      const token = await getToken();
+      const response = await fetch(`${API_URL}/api/match-report/${matchReportId}/accept`, {
+        method: 'PATCH',
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error(data.error || 'Failed to accept match results');
+      }
+
+      setPendingMatchReport(null);
+      setRenderUser(prev => prev + 1);
+
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  const handleDecline = async (matchReportId: string) => {
+    try {
+      const token = await getToken();
+      const response = await fetch(`${API_URL}/api/match-report/${matchReportId}/decline`, {
+        method: 'PATCH',
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error(data.error || 'Failed to decline match results');
+      }
+
+      setPendingMatchReport(null);
+      setRenderUser(prev => prev + 1);
+
+    } catch (error) {
+      console.log(error);
+      throw error
     }
   }
 
@@ -51,7 +135,6 @@ export function HomePage() {
       try {
         // get session token from user
         const token = await getToken();
-
         // send req to backend api to obtain user data to display
         const response = await fetch(`${API_URL}/api/user`, {
           method: 'GET',
@@ -59,17 +142,17 @@ export function HomePage() {
             Authorization: `Bearer ${token}`,
           },
         });
-
-        const data = await response.json()
-        const { needsOnboarding, user } = data
-
-        console.log(needsOnboarding);
-        console.log(user);
+  
+        const data = await response.json();
+        if (!response.ok) {
+          throw new Error(data.error || 'Failed to get user');
+        }
+        const { needsOnboarding, user } = data;
         setNeedsOnboarding(needsOnboarding);
         if (!needsOnboarding) {
           setUser(user);
         }
-
+  
       } catch (error) {
           console.log(error);
       } finally {
@@ -77,8 +160,36 @@ export function HomePage() {
       }
     }
 
-    fetchUser()
-  }, [needsOnboarding])
+    const fetchPendingMatchReport = async () => {
+      try {
+        const token = await getToken();
+        const response = await fetch(`${API_URL}/api/match-report/pending`, {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+  
+        const data = await response.json();
+        if (!response.ok) {
+          throw new Error(data.error || 'Failed to get pending match report');
+        }
+        const { matchReport } = data;
+        setPendingMatchReport(matchReport);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    
+    fetchUser();
+    fetchPendingMatchReport();
+    const interval = setInterval(() => {
+      fetchPendingMatchReport(); 
+    }, 5000)
+
+    return () => clearInterval(interval);
+  }, [renderUser])
 
   if (loading) {
     return (
@@ -86,7 +197,7 @@ export function HomePage() {
         <p>Loading...</p>
       </>
     )
-  } 
+  }
 
   if (needsOnboarding) {
     return (
@@ -94,23 +205,45 @@ export function HomePage() {
         <OnboardingForm onSubmitUsername={handleCreateUser}/>
       </>
     )
-  } else {
+  }
+
+  if (reportingMatch) {
     return (
       <>
-        <div className="home-page">
-          <img className="user-profile-pic" src={user.profilePicture} />
-          <h2 className="user-username">{user.username}</h2>
-          <div className="container">
-            <img className="user-rank-image" src={`/images/rankedpool-${user.rank}.png`} alt={`${user.rank} image`}/>
-            <div className="elo-bar">
-              <div className="elo-fill" style={{width: `${user.elo}%`}}></div>
-            </div>
-            <p className="elo-rating">{`${user.elo}`} / 100</p>
-            <button className="report-match">Report Match</button>
-          </div>
-          <MenuBar />
-        </div>
+        <MatchReportForm onSubmitMatchReport={handleCreateMatchReport}
+          onClose={() => setReportingMatch(false)}
+        />
       </>
     )
   }
+
+  if (pendingMatchReport !== null) {
+    return (
+      <>
+        <PendingMatchReport 
+          newMatchReport={pendingMatchReport}
+          handleAccept={handleAccept}
+          handleDecline={handleDecline}
+        />
+      </>
+    )
+  }
+
+  return (
+    <>
+      <div className="home-page">
+        <img className="user-profile-pic" src={user.profilePicture} />
+        <h2 className="user-username">{user.username}</h2>
+        <div className="container">
+          <img className="user-rank-image" src={`/images/rankedpool-${user.rank}.png`} alt={`${user.rank} image`}/>
+          <div className="elo-bar">
+            <div className="elo-fill" style={{width: `${user.elo}%`}}></div>
+          </div>
+          <p className="elo-rating">{`${user.elo}`} / 100</p>
+          <button className="report-match" onClick={() => setReportingMatch(true)}>Report Match</button>
+        </div>
+        <MenuBar />
+      </div>
+    </>
+  )
 }
